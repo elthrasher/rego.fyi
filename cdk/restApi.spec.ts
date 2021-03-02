795d4f1f9e94dcd2077b7129bca12cff71408b6b
elthrasher <elthrasher@gmail.com>
2021-03-02 15:58:04 -0500

    tests

--- cdk/restApi.spec.ts
+++ cdk/restApi.spec.ts
@@ -1,13 +1,15 @@
 import { SynthUtils } from '@aws-cdk/assert';
 import { App, Stack } from '@aws-cdk/core';
+import { getCertAndZone } from './getCFAndZone';
 import { getFns } from './lambda';
 import { createRestApi } from './restApi';
 
 describe('rest api', () => {
   test('rest api', () => {
     const app = new App();
-    const stack = new Stack(app);
-    createRestApi(stack, getFns(stack));
+    const stack = new Stack(app, 'ApiTestStack', { env: { account: '123456789', region: 'us-east-1' } });
+    const { certificate, hostedZone } = getCertAndZone(stack);
+    createRestApi(stack, getFns(stack), certificate, hostedZone);
     const cfn = SynthUtils.toCloudFormation(stack);
     const resources = cfn.Resources;
     const matchObject: { Parameters: Record<string, unknown>; Resources: Record<string, unknown> } = {
--- cdk/website.spec.ts
+++ cdk/website.spec.ts
@@ -1,13 +1,16 @@
 import { SynthUtils } from '@aws-cdk/assert';
 import { App, Stack } from '@aws-cdk/core';
 
+import { getCertAndZone } from './getCFAndZone';
 import { createWebsite } from './website';
 
 describe('website builds', () => {
   test('react build', () => {
     const app = new App();
     const stack = new Stack(app, 'WebTestStack', { env: { account: '123456789', region: 'us-east-1' } });
-    createWebsite(stack);
+    const { certificate, hostedZone } = getCertAndZone(stack);
+
+    createWebsite(stack, certificate, hostedZone);
     const cfn = SynthUtils.toCloudFormation(stack);
     const resources = cfn.Resources;
     const matchObject: { Parameters: Record<string, unknown>; Resources: Record<string, unknown> } = {
